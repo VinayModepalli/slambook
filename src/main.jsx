be8c824309d5575ffd7496c28c38dfb837136517
VinayModepalli <vinaykumarmodepalli@gmail.com>
2025-04-06 15:08:47 +0530

    Add React Router 7.5

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter as Router, Routes, Route} from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <Router>
+          <App />
+          {/*<Routes>*/}
+          {/*    <Route path="/" component={<App />} />*/}
+          {/*</Routes>*/}
+      </Router>
   </StrictMode>,
 )
