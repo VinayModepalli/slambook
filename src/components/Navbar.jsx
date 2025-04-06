feef081ff12b575363dd3230b55e312dd80f564d
VinayModepalli <vinaykumarmodepalli@gmail.com>
2025-04-06 16:53:16 +0530

    Add Login and Signup page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,6 @@ import React from 'react';
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-                <div className="navbar-header">
-                    <p className="navbar-brand text-blue-600">Slambook</p>
-                </div>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav">Create</ul>
-                    <ul className="navbar-nav">Log out</ul>
-                </div>
-            </div>
         </nav>
     );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,103 @@
 import React from 'react';
+import friendsImage from '../assets/friends.jpg'
+import slamBook from '../assets/slambook.png'
+import {Link} from "react-router";
 
-function Signup(props) {
+function Signup() {
     return (
-        <div>Sign up</div>
+        <div className="flex flex-col md:flex-row h-screen p-8 sm:p-0">
+            <div className="md:block md:w-1/2 hidden">
+                <img className="h-full w-full object-cover object-bottom"
+                     src={friendsImage}
+                     alt="Friends Image" />
+            </div>
+
+            <div className="md:w-1/2 flex items-center justify-center h-screen sm:p-8">
+                <div className="flex flex-col w-full">
+                    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                        <img
+                            alt="Slambook"
+                            src={slamBook}
+                            className="mx-auto h-35 w-auto"
+                        />
+                        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                            Create your account
+                        </h2>
+                    </div>
+
+                    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form action="#" method="POST" className="space-y-3">
+                            <div>
+                                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                                    Email address
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        required
+                                        autoComplete="email"
+                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                                        Password
+                                    </label>
+                                </div>
+                                <div className="mt-2">
+                                    <input
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        required
+                                        autoComplete="current-password"
+                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label htmlFor="confirm-password" className="block text-sm/6 font-medium text-gray-900">
+                                        Confirm Password
+                                    </label>
+                                </div>
+                                <div className="mt-2">
+                                    <input
+                                        id="confirm-password"
+                                        name="confirm-password"
+                                        type="password"
+                                        required
+                                        autoComplete="confirm-password"
+                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="mt-5">
+                                <button
+                                    type="submit"
+                                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    Sign up
+                                </button>
+                            </div>
+                        </form>
+
+                        <p className="mt-10 text-center text-sm/6 text-gray-500">
+                            Already have an account?{' '}
+                            <Link to="/login" className="font-semibold text-indigo-600 hover:text-indigo-500">Login</Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     );
 }
 
